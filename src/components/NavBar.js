62eb41ddf6dd8a4bcd619f5315e06652c6059f5b
notPrasantesp17 <punkersyt@gmail.com>
2024-08-12 23:56:23 +0545

    navbar finished

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import { faBell, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
 const NavBar = () => {
   return (
-    <div>
-      <h1 className='border-b-2 shadow-lg h-20 bg-[blueviolet] '>NavBar</h1>
+    <div className="grid grid-cols-10 h-20 border-b-2 shadow-sm ">
+      <div className="col-span-2 flex items-center mx-6">
+        <h2 className="text-xl font-sans font-bold">Dashboard</h2>
       </div>
-  )
-}
+      <div className="col-span-1 "></div>
+      <div className="col-span-7 flex">
+        <div className="  w-2/3 flex items-center justify-center  ">
+          <input
+            className="border-y-2 border-l-2 w-[80%] h-12  px-4 rounded-l-3xl border-[blueviolet]  "
+            placeholder="Search"
+          />
+          <button className="border-r-2 border-y-2 h-12 rounded-r-3xl border-[blueviolet]">
+            <FontAwesomeIcon
+              className="text-2xl px-3 self-center active:text-[blueviolet]"
+              color="gray"
+              icon={faSearch}
+            />
+          </button>
+        </div>
+        <div className=" w-1/3 flex justify-between">
+       <FontAwesomeIcon className="text-3xl m-auto" icon={faBell}/>
+       <div className="w-3/4 flex justify-between">
+        <img className="w-16 h-16 m-auto" src="https://avatars.githubusercontent.com/u/122468711?v=4"/>
+        <span className="m-auto">
+          <h3 className=" font-semibold text-md">Prasant Bhattarai</h3>
+          <h4 className="text-[gray] text-md">@prasantesp17</h4>
+        </span>
+       </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,46 +1,141 @@
-import React from 'react'
-import Logo from "../utils/pw.png"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBorderAll, faCreditCard, faDollar, faEuro, faGear, faMoneyBill, faMoneyBillTransfer, faMoneyCheckDollar, faPiggyBank, faQuestion, faSignOut} from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import Logo from "../utils/pw.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBorderAll,
+  faCreditCard,
+  faDollar,
+  faGear,
+  faMoneyBillTransfer,
+  faMoneyCheckDollar,
+  faPiggyBank,
+  faQuestion,
+  faSignOut,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
-  const ree='red';
+  const [selectedItem, setSelectedItem] = useState("overview");
+  const handleClick = (item) => {
+    setSelectedItem(item);
+  };
+  const getClassForItem = (item) => {
+    return selectedItem == item
+      ? "cursor-pointer bg-[blueviolet] rounded-md text-white"
+      : "OverallHover";
+  };
   return (
-    <div className='flex flex-col space-y-32 border-r-2 shadow-md '>
+    <div className="flex flex-col space-y-32 shadow-md ">
       <div>
-    <div className=' flex h-20 border-b-2 mb-6'>
- <img className=' h-full w-full cursor-pointer' src={Logo} alt='Logo'/>
+        <div className=" flex h-20 border-b-2 mb-6">
+          <img
+            className=" h-full w-full cursor-pointer"
+            src={Logo}
+            alt="Logo"
+          />
+        </div>
+        <div>
+          <div>
+            <h3 className="font-merriweather text-lg  font-bold my-1 mx-2 p-1">
+              Menu
+            </h3>
+            <ul>
+              <li
+                onClick={() => handleClick("overview")}
+                className={`font-semibold text-md font-sans my-4 mx-4 p-1 ${getClassForItem(
+                  "overview"
+                )} `}
+              >
+                <FontAwesomeIcon className="mx-4" icon={faBorderAll} /> Overview
+              </li>
+              <li
+                onClick={() => handleClick("income")}
+                className={`font-semibold text-md font-sans my-4 mx-4 p-1   ${getClassForItem(
+                  "income"
+                )} `}
+              >
+                <FontAwesomeIcon className="mx-4 mr-6" icon={faDollar} />
+                Income
+              </li>
+              <li
+                onClick={() => handleClick("expenses")}
+                className={`font-semibold text-md font-sans my-4 mx-4 p-1 ${getClassForItem(
+                  "expenses"
+                )} `}
+              >
+                <FontAwesomeIcon className="mx-4" icon={faMoneyCheckDollar} />{" "}
+                Expenses
+              </li>
+              <li
+                onClick={() => handleClick("savings")}
+                className={`font-semibold text-md font-sans my-4 mx-4 p-1  ${getClassForItem(
+                  "savings"
+                )} `}
+              >
+                <FontAwesomeIcon className="mx-4" icon={faPiggyBank} /> Savings
+              </li>
+              <li
+                onClick={() => handleClick("transactions")}
+                className={`font-semibold text-md font-sans my-4 mx-4 p-1 ${getClassForItem(
+                  "transactions"
+                )}  `}
+              >
+                <FontAwesomeIcon className="mx-4" icon={faMoneyBillTransfer} />{" "}
+                Transactions
+              </li>
+              <li
+                onClick={() => handleClick("billings")}
+                className={`font-semibold text-md font-sans my-4 mx-4 p-1 ${getClassForItem(
+                  "billings"
+                )} `}
+              >
+                <FontAwesomeIcon className="mx-4" icon={faCreditCard} />{" "}
+                Billings
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="font-merriweather text-lg  font-bold my-1 mx-2 p-1">
+              General
+            </h3>
+            <ul>
+              <li
+                onClick={() => handleClick("settings")}
+                className={`font-semibold text-md font-sans my-4 mx-4 p-1 ${getClassForItem(
+                  "settings"
+                )}`}
+              >
+                {" "}
+                <FontAwesomeIcon className="mx-4" icon={faGear} />
+                settings
+              </li>
+              <li
+                onClick={() => handleClick("help")}
+                className={`font-semibold   text-md font-sans my-4 mx-4 p-1 ${getClassForItem(
+                  "help"
+                )}`}
+              >
+                {" "}
+                <FontAwesomeIcon className="mx-4" icon={faQuestion} />
+                help
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
       <div>
-  <div>
-    <h3 className='font-merriweather text-lg  font-bold my-1 mx-2 p-1'>Menu</h3>
- <ul>
-  <li className='font-semibold text-md font-sans my-4 mx-4 p-1  cursor-pointer bg-[blueviolet] rounded-md text-white'><FontAwesomeIcon className='mx-4' icon={faBorderAll}/> Overview</li>
-  <li className='font-semibold text-md font-sans my-4 mx-4 p-1 OverallHover'><FontAwesomeIcon className='mx-4 mr-6' icon={faDollar}/>Income</li>
-  <li className='font-semibold text-md font-sans my-4 mx-4 p-1 OverallHover'><FontAwesomeIcon className='mx-4' icon={faMoneyCheckDollar}/> Expenses</li>
-  <li className='font-semibold text-md font-sans my-4 mx-4 p-1 OverallHover'><FontAwesomeIcon className='mx-4' icon={faPiggyBank}/> Savings</li>
-  <li className='font-semibold text-md font-sans my-4 mx-4 p-1 OverallHover'><FontAwesomeIcon className='mx-4' icon={faMoneyBillTransfer}/> Transactions</li>
-  <li className='font-semibold text-md font-sans my-4 mx-4 p-1 OverallHover'><FontAwesomeIcon className='mx-4' icon={faCreditCard}/> Billings</li>
- </ul>
-  </div>
-  <div>
-    <h3 className='font-merriweather text-lg  font-bold my-1 mx-2 p-1'>General</h3>
-    <ul>
-    <li className='font-semibold text-md font-sans my-4 mx-4 p-1 OverallHover'> <FontAwesomeIcon  className='mx-4' icon={faGear}/>settings</li>
-  <li className='font-semibold   text-md font-sans my-4 mx-4 p-1 OverallHover'> <FontAwesomeIcon  className='mx-4' icon={faQuestion}/>help</li>
-    </ul>
-  </div>
+        <h3
+          onClick={() => handleClick("logout")}
+          className={`font-semibold text-lg font-merriweather my-4 mx-4 p-1 ${getClassForItem(
+            "logout"
+          )}`}
+        >
+          {" "}
+          <FontAwesomeIcon className="mx-4" icon={faSignOut} />
+          logout
+        </h3>
       </div>
     </div>
-  <div>
-   <h3 className='font-semibold text-lg font-merriweather my-4 mx-4 p-1 OverallHover' > <FontAwesomeIcon  className='mx-4' icon={faSignOut}/>logout</h3>
-  </div>
+  );
+};
 
-
-    </div>
-
-
-  )
-}
-
-export default Sidebar
+export default Sidebar;
