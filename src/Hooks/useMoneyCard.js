cab22fa647977e15f5ade834a8d0756f2f290b4a
notPrasantesp17 <punkersyt@gmail.com>
2024-08-20 09:44:06 +0545

    expenses card are geetting the data

--- src/Hooks/useMoneyCard.js
+++ src/Hooks/useMoneyCard.js
@@ -161,5 +161,166 @@ export const UseIncomeCard = (token) => {
   return { OData, loading, error };
 };
 
+export const useExpenseCard = (token) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const addCards = async () => {
+    try {
+      const response = await fetch("/expense/totalexpense", {
+        method: 'GET',
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      addCards();
+    }
+  }, [token]);
+
+  return { data, loading, error };
+};
+ export const useGroceryCard = (token) => {
+  const [Gdata, setGData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const addCards = async () => {
+    try {
+      const response = await fetch("/expense/groceriescard", {
+        method: 'GET',
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      setGData(result);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      addCards();
+    }
+  }, [token]);
+
+  return { Gdata, loading, error };
+};
+ export const useBillsCard = (token) => {
+  const [Bdata, setBdata] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const addCards = async () => {
+    try {
+      const response = await fetch("/expense/billscard", {
+        method: 'GET',
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      setBdata(result);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      addCards();
+    }
+  }, [token]);
+
+  return { Bdata, loading, error };
+};
+ export const useEntertainmentCard = (token) => {
+  const [EnterData, setEnterData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const addCards = async () => {
+    try {
+      const response = await fetch("/expense/entertaintmentcard", {
+        method: 'GET',
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      setEnterData(result);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      addCards();
+    }
+  }, [token]);
+
+  return { EnterData, loading, error };
+};
+ export const useOthersExpcard = (token) => {
+  const [OEData, setOEData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const addCards = async () => {
+    try {
+      const response = await fetch("/expense/otherscard", {
+        method: 'GET',
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      setOEData(result);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      addCards();
+    }
+  }, [token]);
+
+  return { OEData, loading, error };
+};
+
 
 
--- src/components/ExpenseCard.js
+++ src/components/ExpenseCard.js
@@ -8,13 +8,15 @@ import {
   } from "@fortawesome/free-solid-svg-icons";
   import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
   import React from "react";
-import {UseIncomeCard} from "../Hooks/useMoneyCard";
+import {useBillsCard, useEntertainmentCard, useGroceryCard, useOthersExpcard} from "../Hooks/useMoneyCard";
 
 const ExpensesCard = () => {
 
 const token =localStorage.getItem('token')
- const { data} = UseIncomeCard(token);
-const income=data?.total|| 0.00;
+const { Gdata} = useGroceryCard(token);
+const { Bdata} = useBillsCard(token);
+const { EnterData} = useEntertainmentCard(token);
+const { OEData} = useOthersExpcard(token);
 
 
   return (
@@ -28,12 +30,12 @@ const income=data?.total|| 0.00;
           <FontAwesomeIcon className="mx-1" icon={faBreadSlice} /> Groceries
         </h3>
         <span className="flex mx-5 my-5 space-x-3">
-          <h1 className="text-5xl font-bold font-sans">${income}</h1>
-          <p className="mt-7 text-green-400 bg-gray-100 rounded-md flex "><FontAwesomeIcon className="mt-1 mr-1" icon={faArrowUp}/>12.2%</p>
+          <h1 className="text-5xl font-bold font-sans">${Gdata?.total}</h1>
+          <p className="mt-7 text-green-400 bg-gray-100 rounded-md flex "><FontAwesomeIcon className="mt-1 mr-1" icon={faArrowUp}/>{Gdata?.percentage}%</p>
         </span>
         <span className="flex mx-6 space-x-1">
-          <p className=" text-green-400 bg-gray-100 rounded-md flex ">+$112</p>
-          <p className="font-sans font-semibold text-md text-gray-500">than last month</p>
+          <p className=" text-green-400 bg-gray-100 rounded-md flex text-sm ">{Gdata?.message}</p>
+          <p className="font-sans font-semibold text-md text-gray-500 text-sm">than last month</p>
         </span>
 
       </div>
@@ -43,12 +45,12 @@ const income=data?.total|| 0.00;
           Bills
         </h3>
         <span className="flex mx-5 my-5 space-x-3">
-          <h1 className="text-5xl font-bold font-sans">$928.89</h1>
-          <p className="mt-7 text-green-400 bg-gray-100 rounded-md flex "><FontAwesomeIcon className="mt-1 mr-1" icon={faArrowUp}/>12.2%</p>
+          <h1 className="text-5xl font-bold font-sans">${Bdata?.total}</h1>
+          <p className="mt-7 text-green-400 bg-gray-100 rounded-md flex "><FontAwesomeIcon className="mt-1 mr-1" icon={faArrowUp}/>{Bdata?.percentage}%</p>
         </span>
         <span className="flex mx-6 space-x-1">
-          <p className=" text-green-400 bg-gray-100 rounded-md flex ">+$112</p>
-          <p className="font-sans font-semibold text-md text-gray-500">than last month</p>
+          <p className=" text-green-400 bg-gray-100 rounded-md flex text-sm ">{Bdata?.message}</p>
+          <p className="font-sans font-semibold text-md text-gray-500 text-sm">than last month</p>
         </span>
       </div>
       <div className="w-[88%] border-2 shadow-md m-auto h-40 rounded-xl  col-span-3 border-x-2 bg-[#ffffff]">
@@ -56,12 +58,12 @@ const income=data?.total|| 0.00;
           <FontAwesomeIcon className="mx-1" icon={faFilm} /> Entertainments
         </h3>
         <span className="flex mx-5 my-5 space-x-3">
-          <h1 className="text-5xl font-bold font-sans">$928.89</h1>
-          <p className="mt-7 text-green-400 bg-gray-100 rounded-md flex "><FontAwesomeIcon className="mt-1 mr-1" icon={faArrowUp}/>12.2%</p>
+          <h1 className="text-5xl font-bold font-sans">${EnterData?.total}</h1>
+          <p className="mt-7 text-green-400 bg-gray-100 rounded-md flex "><FontAwesomeIcon className="mt-1 mr-1" icon={faArrowUp}/>{EnterData?.percentage}%</p>
         </span>
         <span className="flex mx-6 space-x-1">
-          <p className=" text-green-400 bg-gray-100 rounded-md flex ">+$112</p>
-          <p className="font-sans font-semibold text-md text-gray-500">than last month</p>
+          <p className=" text-green-400 bg-gray-100 rounded-md flex text-sm ">{EnterData?.message}</p>
+          <p className="font-sans font-semibold text-md text-gray-500 text-sm">than last month</p>
         </span>
       </div>
       <div className="w-[88%] border-2 shadow-md m-auto h-40 rounded-xl   col-span-3 border-x-2 bg-[#ffffff]">
@@ -69,12 +71,12 @@ const income=data?.total|| 0.00;
           <FontAwesomeIcon className="mx-1" icon={faMoneyBills} /> Others
         </h3>
         <span className="flex mx-5 my-5 space-x-3">
-          <h1 className="text-5xl font-bold font-sans">$928.89</h1>
-          <p className="mt-7 text-green-400 bg-gray-100 rounded-md flex "><FontAwesomeIcon className="mt-1 mr-1" icon={faArrowUp}/>12.2%</p>
+          <h1 className="text-5xl font-bold font-sans">${OEData?.total}</h1>
+          <p className="mt-7 text-green-400 bg-gray-100 rounded-md flex "><FontAwesomeIcon className="mt-1 mr-1" icon={faArrowUp}/>{OEData?.percentage}%</p>
         </span>
         <span className="flex mx-6 space-x-1">
-          <p className=" text-green-400 bg-gray-100 rounded-md flex ">+$112</p>
-          <p className="font-sans font-semibold text-md text-gray-500">than last month</p>
+          <p className=" text-green-400 bg-gray-100 rounded-md flex  text-sm">{OEData?.message}</p>
+          <p className="font-sans font-semibold text-md text-gray-500 text-sm">than last month</p>
         </span>
       </div>
     </div>
